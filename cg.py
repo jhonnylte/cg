8b05e5a50c6d7f3e1059e51ad33917f0580d59ba
Jhonny <jhonnyleite.14@gmail.com>
2026-06-12 20:31:57 -0300

    Criação da Classe Objeto3D e do peão com Marching Cubes

--- cg.py
+++ cg.py
@@ -2,9 +2,179 @@ import tkinter as tk
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from skimage.measure import marching_cubes
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
-# Variável global para guardar a posição anterior da câmera
-ultimo_eye = [15.0, 20.0, 15.0]
+def calcular_normais_faces(vertices, faces):
+
+    normais = []
+
+    for face in faces:
+
+        i, j, k = face
+
+        A = vertices[i]
+        B = vertices[j]
+        C = vertices[k]
+
+        AB = B - A
+        AC = C - A
+
+        N = np.cross(AB, AC)
+
+        norma = np.linalg.norm(N)
+
+        if norma != 0:
+            N = N / norma
+
+        normais.append(N)
+
+    return np.array(normais)
+
+def adicionar_CH(Po):
+      coluna_de_uns = np.ones((Po.shape[0], 1), dtype=int)
+      PoCH = np.hstack((Po, coluna_de_uns))
+      return PoCH
+
+class Objeto3D:
+    def __init__(
+        self,
+        vertices=None,
+        arestas=None,
+        faces=None,
+        normais=None
+    ):
+        self.vertices = vertices #or []
+        self.arestas = arestas #or []
+        self.faces = faces #or []
+        self.normais = normais #or []
+
+    def translacao(self, tx, ty, tz):
+      T = np.array([
+          [1, 0, 0, tx],
+          [0, 1, 0, ty],
+          [0, 0, 1, tz],
+          [0, 0, 0, 1]])
+      Pn = np.dot(T, adicionar_CH(self.vertices).T).T
+      Pn = Pn[:, :3]
+      return Objeto3D(vertices=Pn, faces=self.faces, normais=self.normais)
+
+    def rotacao(self, theta, eixo):
+      theta = np.radians(theta)
+      if eixo == 'x':
+        R = np.array([
+            [1, 0, 0,                          0],
+            [0, np.cos(theta), -np.sin(theta), 0],
+            [0, np.sin(theta), np.cos(theta),  0],
+            [0, 0, 0,                          1]])
+      elif eixo == 'y':
+        R = np.array([
+            [ np.cos(theta), 0, np.sin(theta), 0],
+            [0, 1, 0,                          0],
+            [-np.sin(theta), 0, np.cos(theta), 0],
+            [0, 0, 0,                          1]
+        ])
+      elif eixo == 'z':
+        R = np.array([
+            [np.cos(theta), -np.sin(theta), 0, 0],
+            [np.sin(theta),  np.cos(theta), 0, 0],
+            [0,0,1,0],
+            [0,0,0,1]
+        ])
+
+      Pn = np.dot(R, adicionar_CH(self.vertices).T).T
+      Pn = Pn[:, :3]
+
+      self.atualizar_normais()
+      return Objeto3D(vertices=Pn, faces=self.faces, normais=self.normais)
+
+    def escala(self, sx, sy, sz):
+      S = np.array([
+          [sx, 0, 0,  0],
+          [0, sy, 0,  0],
+          [0, 0,  sz, 0],
+          [0, 0,  0,  1]
+      ])
+      Pn = np.dot(S, adicionar_CH(self.vertices).T).T
+      Pn = Pn[:, :3]
+      return Objeto3D(vertices=Pn, faces=self.faces, normais=self.normais)
+
+    def transformar_Scc(self, ViewMatrix):
+      Pn = np.dot(ViewMatrix, adicionar_CH(self.vertices).T).T
+      Pn = Pn[:, :3]
+      objetoScc = Objeto3D(
+          vertices = Pn,
+          normais = self.normais,
+          faces = self.faces
+      )
+      return objetoScc
+
+    def atualizar_normais(self):
+      self.normais = calcular_normais_faces(self.vertices, self.faces)
+
+
+""" CRIAÇÃO DO PEÃO COM MARCHING CUBES """
+N = 8
+
+x = np.linspace(-4, 4, N)
+y = np.linspace(0, 12, N)
+z = np.linspace(-4, 4, N)
+
+X, Y, Z = np.meshgrid(
+    x, y, z,
+    indexing='ij'
+)
+
+cilindro = np.sqrt(X**2 + Z**2) - 3
+cilindro[Y > 2] = 1
+
+raio_tronco = 2 - (Y - 4)/4
+tronco = np.sqrt(X**2 + Z**2) - raio_tronco
+tronco[(Y < 2) | (Y > 8)] = 1
+
+esfera = np.sqrt(X**2 + (Y - 9.5)**2 + Z**2) - 2
+
+volume = np.minimum(cilindro, tronco)
+volume = np.minimum(volume, esfera)
+
+peao_vertices, peao_faces, peao_normais, _ = marching_cubes(
+    volume,
+    level=0
+)
+
+peao_vertices[:,0] = np.interp(
+    peao_vertices[:,0],
+    [0, N-1],
+    [x.min(), x.max()]
+)
+
+peao_vertices[:,1] = np.interp(
+    peao_vertices[:,1],
+    [0, N-1],
+    [y.min(), y.max()]
+)
+
+peao_vertices[:,2] = np.interp(
+    peao_vertices[:,2],
+    [0, N-1],
+    [z.min(), z.max()]
+)
+
+peao = Objeto3D(
+    vertices=peao_vertices,
+    faces=peao_faces,
+    normais=peao_normais
+)
+""""====================PEÃO CRIADO COM MARCHING CUBES===================="""
+"""======================================================================="""
+
+
+
+
+
+
+""" Variável global para guardar a posição anterior da câmera """
+ultimo_eye = [10.0, 20.0, 10.0]
 
 def atualizar_grafico():
     global ultimo_eye
@@ -45,13 +215,36 @@ def atualizar_grafico():
     eixo_3d.cla()
 
     # 1. Trajetória (Espiral)
-    t = np.linspace(0, 20, 200)
-    eixo_3d.plot(np.sin(t), np.cos(t), t, color='teal', linewidth=2.5, alpha=0.5, label='Trajetória')
+    #t = np.linspace(0, 20, 200)
+    #eixo_3d.plot(np.sin(t), np.cos(t), t, color='teal', linewidth=2.5, alpha=0.5, label='Trajetória')
 
     # 2. Desenhar Alvo e Câmera
-    eixo_3d.scatter(ax_at, az_at, ay_at, color='red', s=150, marker='.', label='At (Alvo)')
-    eixo_3d.scatter(ex, ez, ey, color='black', s=100, marker='.', label='Eye (Câmera)')
-    eixo_3d.scatter(0, 0, 0, color='green', s=100, marker='.', label='Origem (0,0,0)')
+    eixo_3d.scatter(ax_at, az_at, ay_at, color='red', s=150, marker='.', label='')
+    eixo_3d.scatter(ex, ez, ey, color='black', s=100, marker='.', label='')
+    eixo_3d.scatter(0, 0, 0, color='green', s=200, marker='.', label='Origem (0,0,0)')
+
+    """ Desenhar o Peão usando os vértices gerados pelo marching cubes """
+    Vpeao = peao.vertices
+
+    triangulosPeao = []
+
+    for face in peao.faces:
+
+        triangulo = peao.vertices[face]
+
+        triangulo = triangulo[:, [0,2,1]]
+
+        triangulosPeao.append(triangulo)
+
+    malhaPeao = Poly3DCollection(
+        triangulosPeao,
+        edgecolor='blue',
+        linewidth=0.2,
+        alpha=0.8
+    )
+
+    eixo_3d.add_collection3d(malhaPeao)
+
     # 3. Caixa de Visualização (Frustum)
     E = np.array([ex, ez, ey])      
     A = np.array([ax_at, az_at, ay_at]) 
